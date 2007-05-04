8a9cea9b88ab2900b7c7fd9d463d1f1ba79f6a63
Yuval Kogman <nothingmuch@woobling.org>
2007-05-04 19:07:28 +0000

    accept objects in ->{isa} too

--- lib/MooseX/AttributeHelpers/Collection/Array.pm
+++ lib/MooseX/AttributeHelpers/Collection/Array.pm
@@ -58,7 +58,8 @@ sub _process_options_for_provides {
     (exists $options->{isa})
         || confess "You must define a type with the Array metaclass";  
          
-    (find_type_constraint($options->{isa})->is_a_type_of('ArrayRef'))
+    my $c = find_type_constraint($options->{isa}) || $options->{isa};
+    ( $c && blessed($c) && $c->is_a_type_of('ArrayRef'))
         || confess "The type constraint for a Array ($options->{isa}) must be a subtype of ArrayRef";
 }
 
